399bd411fa3366086c406049fefb92b2c2aebdd8
Roy Ayush <ayush2006kumar996@gmail.com>
2025-08-31 21:13:32 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,1 @@
-import Head from "next/head";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Rupeeflow</title>
-        <meta name="description" content="Welcome to Rupeeflow!" />
-      </Head>
-      <main style={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-        <h1>Welcome to Rupeeflow</h1>
-        <p>Your application is running successfully!</p>
-      </main>
-    </>
-  );
-}
